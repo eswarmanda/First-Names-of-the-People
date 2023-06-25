5f3d06777bef3f9e49a11311ef216c684c3a15ed
eswarmanda <eswar95manda@gmail.com>
2023-06-25 11:39:11 +0000

    first name

---
+++ index.js
@@ -0,0 +1,11 @@
+const peopleNames = require('../country/state/city/index');
+const getFirstName = require('../utilities/utils/index')
+
+
+const getPeopleInCity = (peopleNames) => {
+    return getFirstName(peopleNames);
+};
+
+
+module.exports = getPeopleInCity; 
+console.log(getPeopleInCity(peopleNames))
